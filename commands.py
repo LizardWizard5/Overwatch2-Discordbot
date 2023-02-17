771ce82a0622fb7e3fd55e7fb0c36c4e8ec55a27
LizardWizard <ericsamonkey@gmail.com>
2023-02-17 13:51:36 -0500

    Added error checking and new error image

--- commands.py
+++ commands.py
@@ -30,11 +30,15 @@ def playerGrab(name,platform):#
     req = requests.get(f"https://overfast-api.tekrop.fr/players/{name}/summary")
     data = req.text
     jdata = json.loads(data)
+    try:
     #There actually has to be a faster method to do this but it's too late for my brain to process the research
-    data = f"**Name: {jdata['username']}\nTitle: {jdata['title']}\n\
+        data = f"**Name: {jdata['username']}\nTitle: {jdata['title']}\n\
          EndorsmentLVL: {jdata['endorsement']['level']}\n\
             RANKED\n\
             Tank:   {jdata['competitive'][platform]['tank']['division']}    {jdata['competitive'][platform]['tank']['tier']}\n\
             DPS:    {jdata['competitive'][platform]['damage']['division']}  {jdata['competitive'][platform]['damage']['tier']}\n\
             Support:{jdata['competitive'][platform]['support']['division']} {jdata['competitive'][platform]['support']['tier']}**"
+    except:
+        return "There was an error grabbing info, make sure you have the right name and the profile is set to public"
     return data
+   
